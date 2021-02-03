25522c585d7a5508000bcb0b7c774a743d84f5bd
George-Pereira <fernanr8@gmail.com>
2021-02-03 13:37:39 -0300

    Fixes

--- src/test/java/com/fernando/livraria/LivrariaApplicationTests.java
+++ src/test/java/com/fernando/livraria/LivrariaApplicationTests.java
@@ -2,9 +2,8 @@ package com.fernando.livraria;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootApplication
 class LivrariaApplicationTests {
 
 	@Test
